8818fe594643489242462c52ed330ba6ee87b1ae
DiegoJesusP <spartfomi@outlook.com>
2025-04-01 11:06:20 -0600

    feat(login-home): :sparkles: feat: add login and home pages
    Added login, reset password, and home pages to the application.  Also integrated email validation using a custom hook.  Implemented routing with React Router to navigate between pages.
    
    No breaking changes.

---
+++ SACIT/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <h1>Pagina Home</h1>
+    </div>
+  );
+};
+
+export default Home;
