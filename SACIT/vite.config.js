3bd1c83a38dab8c97fbcb5409ee5bd0d59023ffe
DiegoJesusP <spartfomi@outlook.com>
2025-04-11 01:24:14 -0600

    feat(sonarqube): :wrench: add SonarQube integration for test coverage
    Configured SonarQube. Added the necessary configuration for the SonarQube scanner to run and provide coverage reports.
    
    No breaking changes.

--- SACIT/vite.config.js
+++ SACIT/vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.js',
+    coverage: {
+      reporter: ['text', 'lcov'],
+      reportsDirectory: 'coverage',
+    },
+  },
 })
